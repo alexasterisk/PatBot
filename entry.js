002970e32cddf08e0c3c8ab1bca77d58c7c4d219
Warfurret <31458482+warfurret@users.noreply.github.com>
2021-08-02 07:09:03 -0400

    use pet-pet-gif

--- entry.js
+++ entry.js
@@ -1,13 +1,19 @@
-const users = require('./userEmojis.json');
+const users = require("./users.json");
 const lastMessageTime = {};
 
 require('dotenv').config();
-const {Client} = require("discord.js");
+const chalk = require("chalk");
+const petpet = require("pet-pet-gif");
+const {Client, MessageAttachment} = require("discord.js");
+
 var client = new Client();
 
-client.once('ready', () => {
-    for (const [uid, _] of Object.entries(users)) {
-        lastMessageTime[uid] = Date.now();
+client.once("ready", async () => {
+    console.log(`${chalk.bgRed('ON')} PatBot has started!`);
+    console.log(`${chalk.bold('Current Epoch:')} ${chalk.yellow(Date.now().toString())}`);
+    for (const userId of users) {
+        console.log(`${chalk.bgGreen('SET')} Setting User ${chalk.green(userId)} @ ${chalk.yellow(Date.now().toString())}`);
+        lastMessageTime[userId] = Date.now();
     };
 
     client.user.setPresence({
@@ -17,15 +23,24 @@ client.once('ready', () => {
         },
         status: "idle"
     });
+
+    console.log(`${chalk.bgMagenta('STATUS')} Set status! We're good to go!`);
 });
 
-client.on("message", message => {
-    var uid = message.author.id;
-    if (users[uid]) {
-        if (Date.now() - lastMessageTime[uid] >= 3600000) {
-            message.channel.send(`Welcome back <@${uid}>! <a:${users[uid]}>\nYou were last online <t:${lastMessageTime[uid].toString().slice(0, -3)}:R>!`);
+client.on("message", async (message) => {
+    var userId = message.author.id;
+    if (users.indexOf(userId) >= 0) {
+        console.log(`Difference in time for ${message.author.username} is ${chalk.yellow((Date.now() - lastMessageTime[userId]).toString())}`);
+
+        if (Date.now() - lastMessageTime[userId] >= 3600000) {
+            console.log(`${chalk.bgGreen('PREV')} It has been longer than an hour since ${message.author.username} has sent a message!`);
+            let animatedGif = await petpet(message.author.displayAvatarURL({"format": "png"}));
+
+            message.channel.send(`Welcome back <@${userId}>!\nYou were last online <t:${lastMessageTime[userId].toString().slice(0, -3)}:R>.`, {
+                files: [new MessageAttachment(animatedGif, "patpat.gif")]
+            });
         };
-        lastMessageTime[uid] = Date.now();
+        lastMessageTime[userId] = Date.now();
     };
 });
 

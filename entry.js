57b9ede19ec816cbe361ad2dc7963b431640f324
Warfurret <31458482+warfurret@users.noreply.github.com>
2021-08-02 17:58:38 -0400

    add chalk

--- entry.js
+++ entry.js
@@ -1,17 +1,31 @@
-const users = require("./users.json");
+let debugTime = Date.now();
+
+const config = require("./config.json");
 const lastMessageTime = {};
 
 require('dotenv').config();
+const chalk = require("chalk");
 const petpet = require("pet-pet-gif");
 const {Client, MessageAttachment} = require("discord.js");
+console.log(`Got request to start @ ${chalk.yellow(debugTime)}`);
+console.log(`Got all dependencies in ${chalk.yellow(Date.now() - debugTime)}ms`);
+debugTime = Date.now();
 
 var client = new Client({ws: {intents: ['GUILD_PRESENCES', 'GUILD_MEMBERS']}});
 
 client.once("ready", async () => {
-    for (const userId of users) {
-        lastMessageTime[userId] = Date.now();
+    console.log(`\n--- Beginning ready state @ ${chalk.yellow(Date.now())} ${chalk.gray(`[Elapsed ${chalk.bgYellow(Date.now() - debugTime)}ms]`)} ---`);
+    debugTime = Date.now();
+    if (!config.allUsers) {
+        for (const userId of config.users) {
+            lastMessageTime[userId] = Date.now();
+            console.log(`* Defined ${chalk.green(userId)} in ${Date.now() - debugTime}ms`);
+            debugTime = Date.now();
+        };
     };
 
+    console.log(`\nSetting presence @ ${chalk.yellow(Date.now())} ${chalk.gray(`[Elapsed ${chalk.bgYellow(Date.now() - debugTime)}ms]`)}`);
+    debugTime = Date.now();
     client.user.setPresence({
         activity: {
             name: "god is dead and i killed him",
@@ -19,26 +33,36 @@ client.once("ready", async () => {
         },
         status: "idle"
     });
+
+    console.log(`--- Finished ready state in ${chalk.yellow(Date.now() - debugTime)}ms ---`);
+    debugTime = Date.now();
 });
 
+
 client.on("presenceUpdate", async (oldMember, newMember) => {
+    console.log(`--- Got presenceUpdate request from ${chalk.green(newMember.user.id)} ---`);
     if (newMember.user.bot) return;
-    var channel = await client.channels.fetch("870918853402701858");
+    var channel = await client.channels.fetch(config.channel);
     var userId = newMember.member.id;
-    if (users.indexOf(userId) >= 0 && newMember.status === "online" && (oldMember && oldMember.status === "offline" || true)) {
-        if (newMember.status === "online" && (oldMember && oldMember.status === "offline" || true)) {
+    if (config.allUsers || config.users.indexOf(userId) >= 0) {
+        console.log(`* ${chalk.green(newMember.user.id)} is in ${chalk.gray('config.json#users')}`);
+        if (newMember.status === "online" && (!oldMember || !oldMember.status)) {
+            console.log(`* ${chalk.green(newMember.user.id)} has gone online!`);
             var animatedGif = await petpet(newMember.user.displayAvatarURL({"format": "png"}));
             var attachment = new MessageAttachment(animatedGif, "patpat.gif");
-            attachment.height = 32;
-            attachment.width = 32;
+            attachment.height = 128;
+            attachment.width = 128;
             channel.send(`Welcome back <@${userId}>!\nYou were last online <t:${lastMessageTime[userId].toString().slice(0, -3)}:R>.`, {
                 files: [attachment]
-            }).then(msg => msg.delete({timeout: 20000}));
+            }).then(msg => {try {msg.delete({timeout: 20000})} catch {}} );
             lastMessageTime[userId] = Date.now();
-        } else if ((newMember.status === "idle" || newMember.status === "offline") && (oldMember && oldMember.status === "online" || true)) {
+        } else if ((!newMember || !newMember.status || newMember.status === "idle")) {
+            console.log(`* ${chalk.green(newMember.user.id)} has gone inactive/offline!`);
             lastMessageTime[userId] = Date.now();
         };
     };
 });
 
 client.login(process.env.token);
+console.log(`Logged in! Took ${chalk.yellow(Date.now() - debugTime)}ms`);
+debugTime = Date.now();

d921a014051ae78238eeaca429c77a6aa60618a8
Warfurret <31458482+warfurret@users.noreply.github.com>
2021-08-02 08:23:23 -0400

    aeaeaeae

--- entry.js
+++ entry.js
@@ -2,17 +2,13 @@ const users = require("./users.json");
 const lastMessageTime = {};
 
 require('dotenv').config();
-const chalk = require("chalk");
 const petpet = require("pet-pet-gif");
 const {Client, MessageAttachment} = require("discord.js");
 
-var client = new Client();
+var client = new Client({ws: {intents: ['GUILD_PRESENCES', 'GUILD_MEMBERS']}});
 
 client.once("ready", async () => {
-    console.log(`${chalk.bgRed('ON')} PatBot has started!`);
-    console.log(`${chalk.bold('Current Epoch:')} ${chalk.yellow(Date.now().toString())}`);
     for (const userId of users) {
-        console.log(`${chalk.bgGreen('SET')} Setting User ${chalk.green(userId)} @ ${chalk.yellow(Date.now().toString())}`);
         lastMessageTime[userId] = Date.now();
     };
 
@@ -23,23 +19,20 @@ client.once("ready", async () => {
         },
         status: "idle"
     });
-
-    console.log(`${chalk.bgMagenta('STATUS')} Set status! We're good to go!`);
 });
 
-client.on("message", async (message) => {
-    var userId = message.author.id;
-    if (users.indexOf(userId) >= 0) {
-        console.log(`Difference in time for ${message.author.username} is ${chalk.yellow((Date.now() - lastMessageTime[userId]).toString())}`);
-
-        if (Date.now() - lastMessageTime[userId] >= 3600000) {
-            console.log(`${chalk.bgGreen('PREV')} It has been longer than an hour since ${message.author.username} has sent a message!`);
-            let animatedGif = await petpet(message.author.displayAvatarURL({"format": "png"}));
-
-            message.channel.send(`Welcome back <@${userId}>!\nYou were last online <t:${lastMessageTime[userId].toString().slice(0, -3)}:R>.`, {
-                files: [new MessageAttachment(animatedGif, "patpat.gif")]
-            });
-        };
+client.on("presenceUpdate", async (oldMember, newMember) => {
+    if (newMember.user.bot) return;
+    var channel = await client.channels.fetch("870918853402701858");
+    var userId = newMember.member.id;
+    if (users.indexOf(userId) >= 0 && newMember.status === "online" && (oldMember && oldMember.status === "offline" || true)) {
+        var animatedGif = await petpet(newMember.user.displayAvatarURL({"format": "png"}));
+        var attachment = new MessageAttachment(animatedGif, "patpat.gif");
+        attachment.height = 32;
+        attachment.width = 32;
+        channel.send(`Welcome back <@${userId}>!\nYou were last online <t:${lastMessageTime[userId].toString().slice(0, -3)}:R>.`, {
+            files: [attachment]
+        }).then(msg => msg.delete({timeout: 20000}));
         lastMessageTime[userId] = Date.now();
     };
 });
